e4e1776c21d1847a0d3c16bdc4e5317702ba4923
Hunter Lindauer <holindauer@gmail.com>
2024-05-01 22:01:03 -0700

    added error handling using Left and Right constructors of Either for Parser to replace returns of Nothing. This fixes the infinite regress on the last commented out test in the last commit

--- compiler/src/Parser.hs
+++ compiler/src/Parser.hs
@@ -1,13 +1,14 @@
 -- Parser.h contains parser functions for the C subset langauge.
 -- The parser is used after the Lexer, and will convert a list of tokens
 -- into an abstract syntax tree (AST) that represents the program's 
--- gramatical structure.
+-- gramatical structure. The AST is defined in AST.hs.
 
 module Parser 
   ( parseProgram
   , parseExpr
   , parseStmt
-  , parseAssignment  
+  , parseAssignment
+  , ParseError(..)   -- Export the ParseError type along with its constructors
   ) where
 
 
@@ -17,21 +18,37 @@ import AST
 -- Type alias for lexed tokens state
 type LexedTokens = [Token]
 
+-- Parser result type alias
+type ParserResult a = Either ParseError a
+
+-- This type is used to represent errors that can occur during parsing
+data ParseError = UnexpectedToken Token
+                | MissingSemicolon
+                | InvalidSyntax String
+                | UnexpectedEndOfInput
+                deriving (Show, Eq)
+
 -- parsePogram is the master parsing function that parses the 
 -- entire program by recursively processing each statement
-parseProgram :: LexedTokens -> ([Stmt], LexedTokens)
-parseProgram [] = ([], []) 
-parseProgram tokens =
-    let (stmt, rest) = parseStmt tokens
-    in case stmt of
-        Nothing -> ([], rest)  -- If parsing fails, stop processing
-        Just s  -> let (nextStmts, finalTokens) = parseProgram rest
-                   in (s : nextStmts, finalTokens)
+-- @dev Left and Right are constructors of the Either type
+parseProgram :: LexedTokens -> ParserResult ([Stmt], LexedTokens)
+parseProgram [] = Right ([], []) -- Base case: empty program
+parseProgram tokens =            -- Recursive case: parse a statement and then the rest of the program
+    case parseStmt tokens of
+        Left err -> Left err     -- error in parsing the statement
+        Right (stmt, rest) ->    -- successfully parsed a statement
+
+            -- Recursively parse the rest of the program
+            case parseProgram rest of
+                Left err -> Left err
+                Right (stmts, finalTokens) -> Right (stmt : stmts, finalTokens)
+
 
 
 -- Determines the type of statement and delegates to the appropriate parsing function
-parseStmt :: LexedTokens -> (Maybe Stmt, LexedTokens)
-parseStmt [] = (Nothing, [])
+-- @dev Stmt is a node type of the AST as defined in AST.hs
+parseStmt :: LexedTokens -> ParserResult (Stmt, LexedTokens)
+parseStmt [] = Left UnexpectedEndOfInput
 parseStmt (t:ts) = case t of
 
     -- Variable declaration statements always start with a data type
@@ -47,48 +64,54 @@ parseStmt (t:ts) = case t of
     TIf -> parseConditionalPlaceholder ts
     TWhile -> parseWhileLoopPlaceholder ts
     TFor -> parseForLoopPlaceholder ts
-    _ -> (Nothing, ts)  -- Unrecognized statements or error handling
+    _ -> Left (InvalidSyntax "Invalid statement")  -- Catch-all for other patterns
 
 --------------------------------------------------------------------------------------------------Assignment Parsing
 
-parseAssignment :: String -> LexedTokens -> (Maybe Stmt, LexedTokens)
-parseAssignment var (TAssign : rest) =
-  case break (== TSemicolon) rest of
-    (exprTokens, semicolon : finalTokens) ->
-      let (maybeExpr, _) = parseExpr exprTokens
-      in case maybeExpr of
-        Just expr -> (Just (AssignStmt var expr), finalTokens)
-        Nothing -> (Nothing, rest)
-    _ -> (Nothing, rest)
-parseAssignment _ _ = (Nothing, [])  -- Catch-all for other patterns
+-- Parses an assignment statement
+parseAssignment :: String -> LexedTokens -> ParserResult (Stmt, LexedTokens)
+parseAssignment var (TAssign : rest) = -- Assignments always start with an ideantifier followed by an assignment operator
+    
+    -- Parse the expression on the right-hand side of the assignment until a semicolon is encountered
+    case break (== TSemicolon) rest of 
+        (exprTokens, semicolon : finalTokens) ->
+
+            -- Parse the expression and create an assignment statement node
+            case parseExpr exprTokens of
+                Right (expr, _) -> Right (AssignStmt var expr, finalTokens)
+                Left err -> Left err
+
+        -- If a semicolon is not found, return an error
+        _ -> Left MissingSemicolon
+
+
 
 --------------------------------------------------------------------------------------------------Expression Parsing
 
 -- Parses an expression from a list of tokens
-parseExpr :: LexedTokens -> (Maybe Expr, LexedTokens)
+parseExpr :: LexedTokens -> ParserResult (Expr, LexedTokens)
 parseExpr tokens = case tokens of
 
-    -- Parse simple assignments of literals or variables 
-    (TIntLit i : rest) -> (Just (IntLit (fromIntegral i)), rest)  -- Convert Int to Integer
-    (TFloatLit f : rest) -> (Just (FloatLit f), rest)
-    (TDoubleLit d : rest) -> (Just (DoubleLit d), rest)
-    (TIdent var : rest) -> (Just (Var var), rest)
+    -- If primary expression, parse it directly
+    (TIntLit i : rest) -> Right (IntLit (fromIntegral i), rest)  
+    (TFloatLit f : rest) -> Right (FloatLit f, rest)           
+    (TDoubleLit d : rest) -> Right (DoubleLit d, rest)           
+    (TIdent var : rest) -> Right (Var var, rest)                
 
-    -- For more complex expressions, delegate to 'parseComplexExpr'
-    _ -> parseComplexExpr tokens  
+    -- If it's not a simple expression, delegate to parseComplexExpr
+    _ -> parseComplexExpr tokens
 
+-- Parses more complex expressions, starting with the lowest precedence
+parseComplexExpr :: LexedTokens -> ParserResult (Expr, LexedTokens)
+parseComplexExpr tokens = parseBinaryExpr 0 tokens -- Start with the lowest precedence level
 
--- Parses complex expressions, starting with the lowest precedence
-parseComplexExpr :: LexedTokens -> (Maybe Expr, LexedTokens)
-parseComplexExpr tokens = parseBinaryExpr 0 tokens  -- Start with lowest precedence level
-
--- Set max prescedene to 6
+-- Set max prescedene to 6 for unary operators
 maxPrecedence :: Int
 maxPrecedence = 6
 
 -- Parses binary expressions based on precedence using recursive descent parsing with presedence climbing
 --
--- Presedence Leves for Binary Operators
+-- Presedence Levels for Binary Operators
 -- Level 0: Logical OR (||)
 -- Level 1: Logical AND (&&)
 -- Level 2: Equality and inequality (==, !=)
@@ -96,109 +119,90 @@ maxPrecedence = 6
 -- Level 4: Addition and subtraction (+, -)
 -- Level 5: Multiplication, division, and modulus (*, /, %)
 -- Level 6: Unary operators (- unary, !, ~)
-parseBinaryExpr :: Int -> LexedTokens -> (Maybe Expr, LexedTokens)
+parseBinaryExpr :: Int -> LexedTokens -> ParserResult (Expr, LexedTokens)
 parseBinaryExpr precedence tokens
 
-    -- If the precedence level is greater than the max, the expression parsed as unary
-    | precedence > maxPrecedence = parseUnaryExpr tokens  
-    | 
-    otherwise = -- It is a binary operator and we need to parse the left and right expressions 
+    -- If the precedence level is greater than the max, parse as a unary expression
+    | precedence > maxPrecedence = parseUnaryExpr tokens
 
-        -- Parse the left-hand side expression w/ a recursive call. This will increment 
-        -- the precence level that will be used when parsing the right-hand side
-        let (lhs, rest) = parseBinaryExpr (precedence + 1) tokens 
-        in case lhs of
+    -- otherwise expression is binary. Recursively parse the left and right sides
+    -- Note that we are increasing the precedence level by 1 for the left side
+    | otherwise = case parseBinaryExpr (precedence + 1) tokens of 
+        
+        -- Parse the left-hand side of the expression
+        Left err -> Left err
+        Right (lhs, rest) -> case parseOperator precedence rest of
 
-            -- If the left-hand side expression is not found, return nothing. This is an error case
-            Nothing -> (Nothing, tokens)
+            -- Parse the operator and the right-hand side of the expression
+            Left err -> Left err
+            Right (op, nextTokens) -> case parseBinaryExpr (precedence + 1) nextTokens of
 
-            -- If something on the left found, check for an operator and parse the right-hand side
-            Just left -> 
-
-                -- Parse the operator based on the current precedence level
-                let (operator, nextTokens) = parseOperator precedence rest
-                in case operator of
-                    Nothing -> (lhs, rest)  -- No operator found, return lhs. This is an error case
-                    Just op ->
-                        -- Parse the right-hand side expression w/ a recursive call
-                        let (rhs, finalTokens) = parseBinaryExpr (precedence + 1) nextTokens
-                        in case rhs of
-
-                            -- If the right-hand side expression is not found, return nothing. This is an error case
-                            Nothing -> (Nothing, tokens)
-
-                            -- Now that we have the left, right, and operator, create a binary operation node
-                            Just right -> (Just (BinOp op left right), finalTokens)
+                -- Parse the right-hand side of the expression and create a binary operation node
+                Left err -> Left err
+                Right (rhs, finalTokens) -> Right (BinOp op lhs rhs, finalTokens)
 
 -- Parses an operator based on the current precedence level
 -- The function returns the AST Op node and the remaining tokens
 -- Parses an operator based on the current precedence level
-parseOperator :: Int -> LexedTokens -> (Maybe Op, LexedTokens)
+parseOperator :: Int -> LexedTokens -> ParserResult (Op, LexedTokens)
 parseOperator precedence tokens = case tokens of
-    (TOr : rest) | precedence == 0 -> (Just Or, rest)
-    (TAnd : rest) | precedence == 1 -> (Just And, rest)
-    (TEqual : rest) | precedence == 2 -> (Just Equal, rest)
-    (TNotEqual : rest) | precedence == 2 -> (Just NotEqual, rest)
-    (TLessThan : rest) | precedence == 3 -> (Just LessThan, rest)
-    (TGreaterThan : rest) | precedence == 3 -> (Just GreaterThan, rest)
-    (TLessEq : rest) | precedence == 3 -> (Just LessEq, rest)
-    (TGreaterEq : rest) | precedence == 3 -> (Just GreaterEq, rest)
-    (TPlus : rest) | precedence == 4 -> (Just Add, rest)
-    (TMinus : rest) | precedence == 4 -> (Just Subtract, rest)
-    (TStar : rest) | precedence == 5 -> (Just Multiply, rest)
-    (TSlash : rest) | precedence == 5 -> (Just Divide, rest)
-    (TPercent : rest) | precedence == 5 -> (Just Modulus, rest)
-    _ -> (Nothing, tokens)
+    (TOr : rest) | precedence == 0 -> Right (Or, rest)
+    (TAnd : rest) | precedence == 1 -> Right (And, rest)
+    (TEqual : rest) | precedence == 2 -> Right (Equal, rest)
+    (TNotEqual : rest) | precedence == 2 -> Right (NotEqual, rest)
+    (TLessThan : rest) | precedence == 3 -> Right (LessThan, rest)
+    (TGreaterThan : rest) | precedence == 3 -> Right (GreaterThan, rest)
+    (TLessEq : rest) | precedence == 3 -> Right (LessEq, rest)
+    (TGreaterEq : rest) | precedence == 3 -> Right (GreaterEq, rest)
+    (TPlus : rest) | precedence == 4 -> Right (Add, rest)
+    (TMinus : rest) | precedence == 4 -> Right (Subtract, rest)
+    (TStar : rest) | precedence == 5 -> Right (Multiply, rest)
+    (TSlash : rest) | precedence == 5 -> Right (Divide, rest)
+    (TPercent : rest) | precedence == 5 -> Right (Modulus, rest)
+    _ -> Left $ UnexpectedToken (head tokens)
+
 
 -- Parses unary expressions
-parseUnaryExpr :: LexedTokens -> (Maybe Expr, LexedTokens)
-parseUnaryExpr (TMinus : rest) =
-    let (expr, finalTokens) = parsePrimaryExpr rest
-    in case expr of
-        Just e -> (Just (UnaryOp Neg e), finalTokens)
-        Nothing -> (Nothing, rest)
+parseUnaryExpr :: LexedTokens -> ParserResult (Expr, LexedTokens)
+parseUnaryExpr (TMinus : rest) = -- Parse unary expressions for negation and logical NOT
+    parsePrimaryExpr rest >>= \(expr, finalTokens) -> Right (UnaryOp Neg expr, finalTokens)
+parseUnaryExpr (TNot : rest) = -- Parse unary expressions for logical NOT
+    parsePrimaryExpr rest >>= \(expr, finalTokens) -> Right (UnaryOp LogicalNot expr, finalTokens)
 
-parseUnaryExpr (TNot : rest) =
-    let (expr, finalTokens) = parsePrimaryExpr rest
-    in case expr of
-        Just e -> (Just (UnaryOp LogicalNot e), finalTokens)
-        Nothing -> (Nothing, rest)
-
+-- If no unary operator is found, parse the primary expression
 parseUnaryExpr tokens = parsePrimaryExpr tokens
 
+
+
+
 -- Parses primary expressions (integers, floats, variables, parenthesized expressions)
-parsePrimaryExpr :: LexedTokens -> (Maybe Expr, LexedTokens)
-parsePrimaryExpr (TLparen : rest) =  -- Handle parenthesized expressions
-    let (expr, nextTokens) = parseExpr rest
-    in case nextTokens of
-        (TRparen : finalTokens) -> (expr, finalTokens) 
-        _ -> (Nothing, nextTokens)
-        
-parsePrimaryExpr tokens = parseExpr tokens  -- Fallback to general expression parsing
-
-
-
+parsePrimaryExpr :: LexedTokens -> ParserResult (Expr, LexedTokens)
+parsePrimaryExpr (TLparen : rest) = 
+    parseExpr rest >>= \(expr, nextTokens) -> case nextTokens of
+        (TRparen : finalTokens) -> Right (expr, finalTokens)
+        _ -> Left MissingSemicolon  -- More specific error if needed
+parsePrimaryExpr tokens = Left $ InvalidSyntax "Invalid primary expression"
 
 
 
 -------------------------------------------------------------------------------------------------- Placeholders for Conditional and Loop Parsing
 
 -- Parses a variable declaration statement
-parseDeclarationPlaceholder :: String -> LexedTokens -> (Maybe Stmt, LexedTokens)
+parseDeclarationPlaceholder :: String -> LexedTokens -> ParserResult (Stmt, LexedTokens)
 parseDeclarationPlaceholder dataType tokens = 
     case tokens of
-        (TIdent var : TSemicolon : rest) -> (Just (Declaration dataType (Var var)), rest)
-        _ -> (Nothing, tokens)  -- Handle error or incomplete declaration
+        (TIdent var : TSemicolon : rest) -> Right (Declaration dataType (Var var), rest)
+        _ -> Left MissingSemicolon
 
 
 -- Placeholder parsing function for conditionals
-parseConditionalPlaceholder :: LexedTokens -> (Maybe Stmt, LexedTokens)
-parseConditionalPlaceholder tokens = (Nothing, tokens)  -- Currently does nothing
+parseConditionalPlaceholder :: LexedTokens -> ParserResult (Stmt, LexedTokens)
+parseConditionalPlaceholder tokens = Left $ InvalidSyntax "Conditional parsing not implemented"
 
 -- Placeholder parsing function for while loops
-parseWhileLoopPlaceholder :: LexedTokens -> (Maybe Stmt, LexedTokens)
-parseWhileLoopPlaceholder tokens = (Nothing, tokens)  -- Currently does nothing
+parseWhileLoopPlaceholder :: LexedTokens -> ParserResult (Stmt, LexedTokens)
+parseWhileLoopPlaceholder tokens = Left $ InvalidSyntax "While loop parsing not implemented"
 
 -- Placeholder parsing function for for loops
-parseForLoopPlaceholder :: LexedTokens -> (Maybe Stmt, LexedTokens)
-parseForLoopPlaceholder tokens = (Nothing, tokens)  -- Currently does nothing
+parseForLoopPlaceholder :: LexedTokens -> ParserResult (Stmt, LexedTokens)
+parseForLoopPlaceholder tokens = Left $ InvalidSyntax "For loop parsing not implemented"
--- compiler/test/ParserSpec.hs
+++ compiler/test/ParserSpec.hs
@@ -1,9 +1,7 @@
 module ParserSpec where
 
--- ParserSpec.hs contains the test cases for the parser functions
-
 import Test.Hspec
-import Parser (parseAssignment, parseProgram, parseExpr, parseStmt)
+import Parser (parseAssignment, parseProgram, parseExpr, parseStmt, ParseError(..))
 import Lexer
 import AST
 
@@ -14,31 +12,31 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Assignment Statement Parsing" $ do
-
-    -- assuming correct lexing of the statment : "x = 10;"
+    -- Assuming correct lexing of the statement: "x = 10;"
     it "parses simple integer assignments correctly" $ do
       let tokens = [TIdent "x", TAssign, TIntLit 10, TSemicolon]
-          expected = Just (AssignStmt "x" (IntLit 10))
-      fst (parseAssignment "x" (tail tokens)) `shouldBe` expected
+      let expected = Right (AssignStmt "x" (IntLit 10), [])
+      parseAssignment "x" (tail tokens) `shouldBe` expected
 
-    -- assuming correct lexing of the statment : "y = 3.14;"
+    -- Assuming correct lexing of the statement: "y = 3.14;"
     it "parses simple float assignments correctly" $ do
       let tokens = [TIdent "y", TAssign, TFloatLit 3.14, TSemicolon]
-          expected = Just (AssignStmt "y" (FloatLit 3.14))
-      fst (parseAssignment "y" (tail tokens)) `shouldBe` expected
+      let expected = Right (AssignStmt "y" (FloatLit 3.14), [])
+      parseAssignment "y" (tail tokens) `shouldBe` expected
 
-    -- assuming correct lexing of the statment : "z = x;"
+    -- Assuming correct lexing of the statement: "z = x;"
     it "parses simple variable assignments correctly" $ do
       let tokens = [TIdent "z", TAssign, TIdent "x", TSemicolon]
-          expected = Just (AssignStmt "z" (Var "x"))
-      fst (parseAssignment "z" (tail tokens)) `shouldBe` expected
+      let expected = Right (AssignStmt "z" (Var "x"), [])
+      parseAssignment "z" (tail tokens) `shouldBe` expected
 
-    -- assuming correct lexing of the statment : "x = 10 + 5;"
+    -- Assuming incorrect lexing of the statement: "x = 10"
     it "fails on missing semicolon in assignment" $ do
       let tokens = [TIdent "x", TAssign, TIntLit 10]
-      fst (parseAssignment "x" (tail tokens)) `shouldBe` Nothing
+      parseAssignment "x" (tail tokens) `shouldBe` Left MissingSemicolon
 
-    -- -- assuming correct lexing of the statment : "x = if;"
-    -- it "fails on invalid right-hand expression" $ do
-    --   let tokens = [TIdent "x", TAssign, TIf, TSemicolon]  -- 'if' is not a valid expression
-    --   fst (parseAssignment "x" (tail tokens)) `shouldBe` Nothing
+    -- Assuming lexing of an invalid right-hand expression: "x = if;"
+    it "fails on invalid right-hand expression" $ do
+        let tokens = [TIdent "x", TAssign, TIf, TSemicolon]
+        parseAssignment "x" (tail tokens) `shouldBe` Left (InvalidSyntax "Invalid primary expression")
+
